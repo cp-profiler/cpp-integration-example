022bc5cae4c3aa0cc94fb1ca5a75071867ba47e0
Maxim Shishmarev <msgmaxim@gmail.com>
2015-09-28 01:39:51 +0800

    removed thread_id from the example as unnecessary; added some comments

--- example.cpp
+++ example.cpp
@@ -11,17 +11,22 @@ int main() {
 
   c.restart(-1, "test example");
 
+  /// First two nodes are constructed
+  /// and sent in one line each
+
   // send root node
-  c.sendNode(0, -1, -1, 2, NodeStatus::BRANCH, "root", -1);
+  c.sendNode(0, -1, -1, 2, NodeStatus::BRANCH, "root");
 
   // send left child
-  c.sendNode(1, 0, 0, 0, NodeStatus::FAILED, "a", -1);
+  c.sendNode(1, 0, 0, 0, NodeStatus::FAILED, "a");
+
+  /// Third node is constructed with
+  /// mandatory fields only, then
+  /// an optional field (label) is specified separately
+  /// and finally the resulting node is sent
 
   Node node(2, 0, 1, 0, NodeStatus::SOLVED);
-
   node.set_label("b");
-  node.set_thread_id(-1);
-
   c.sendNode(node);
 
   c.done();
