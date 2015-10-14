42614920cfc55878abf013b3fa22bfd538adc147
Maxim <maxim.shishmarev@monash.edu>
2015-10-14 13:09:57 +1100

    creating and sending nodes in one line

--- example.cpp
+++ example.cpp
@@ -5,30 +5,29 @@ int main() {
   using Profiling::Node;
   using Profiling::NodeStatus;
 
-  /// Instansiate a Connector with 6565 as port
+  /// Instantiate a Connector with 6565 as port
   Profiling::Connector c(6565);
   c.connect();
 
   /// Starting a new tree (also used in case of a restart)
   c.restart("minimal example");
 
-  /// First two nodes are constructed
-  /// and sent in one line each
+  /// SEND ROOT NODE
 
-  // send root node
-  c.sendNode(0, -1, -1, 2, NodeStatus::BRANCH, "root");
+  /// Arguments:
+  /// 1. 0 -- node index;
+  /// 2. -1 -- parent's index (-1 indicates that the parent does not exist)
+  /// 3. -1 -- parent's current alternative (normally starts with 0, but -1 as irrelevant here)
+  /// 4.  2 -- number of children nodes (as in binary tree)
+  /// 5.  NodeStatus::BRANCH -- decision node / node with children
+  /// The rest of fields are optional and can be specified in arbitrary order
+  c.createNode(0, -1, -1, 2, NodeStatus::BRANCH).set_label("root").send();
 
-  // send left child
-  c.sendNode(1, 0, 0, 0, NodeStatus::FAILED, "a");
+  // SEND LEFT CHILD
+  c.createNode(1, 0, 0, 0, NodeStatus::FAILED).set_label("a").send();
 
-  /// Third node is constructed with
-  /// mandatory fields only, then
-  /// an optional field (label) is specified separately
-  /// and finally the resulting node is sent
-
-  Node node(2, 0, 1, 0, NodeStatus::SOLVED);
-  node.set_label("b");
-  c.sendNode(node);
+  // SEND RIGHT CHILD
+  c.createNode(2, 0, 1, 0, NodeStatus::SOLVED).set_label("b").send();
 
   c.done();
   c.disconnect();
